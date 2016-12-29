64f3d8c6ee20cc079bbd410a10a4b2f07e390bb8
Ryan Freeth <ryan@ryanfreeth.com>
2016-12-28 22:38:01 -0800

    Add travel blog and Webpack

--- config.rb
+++ config.rb
@@ -22,13 +22,40 @@ page '/*.txt', layout: false
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.name = "blog"
+  blog.prefix = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   blog.sources = "blog/{year}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.name = "travel"
+  blog.prefix = "travel"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "{year}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
@@ -63,11 +90,23 @@ activate :directory_indexes
 
 activate :autoprefixer
 
+# Webpack
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ?
+         "./node_modules/webpack/bin/webpack.js --bail -p" :
+         "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+         source: ".tmp/dist",
+         latency: 1
+
 # Build-specific configuration
 configure :build do
   # Exclude any vendor components (bower or custom builds) in the build
-  ignore 'stylesheets/vendor/*'
-  ignore 'javascripts/vendor/*'
+  # ignore 'stylesheets/vendor/*'
+  # ignore 'javascripts/vendor/*'
+  
+  # "Ignore" JS so webpack has full control.
+  ignore { |path| path =~ /\/(.*)\.js$/ && $1 != 'site' }
 
   # Compress ALL images (advanced)
   # Before activating the below, follow setup instructions on https://github.com/toy/image_optim
